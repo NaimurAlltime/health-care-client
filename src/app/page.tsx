fc93ce320ff7918d8e7e30b1c03b35637833f73a
Naimur Rahman <naimur2231@gmail.com>
2024-05-05 05:46:43 -0700

    Project Initialization setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+};
+
+export default HomePage;
