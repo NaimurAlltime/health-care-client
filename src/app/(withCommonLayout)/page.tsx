f09689e136894985047064fa406ab96f2a1166b2
Naimur Rahman <naimur2231@gmail.com>
2024-05-05 11:58:07 -0700

    Creating The Hero Section left side complete

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@mui/material";
+import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-      <Button>Contained</Button>
-    </div>
+    <>
+      <HeroSection />
+    </>
   );
 };
 
