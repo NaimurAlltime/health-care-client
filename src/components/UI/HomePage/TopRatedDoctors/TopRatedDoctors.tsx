8caea7ff38bf0a79a1ad97eab3371bccc61e43d9
Naimur Rahman <naimur2231@gmail.com>
2024-05-06 00:42:14 -0700

    Create Top Rated Doctors Section card design

--- src/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
+++ src/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
@@ -4,13 +4,12 @@ import {
   Card,
   CardActions,
   CardContent,
-  CardMedia,
   Container,
   Grid,
   Typography,
 } from "@mui/material";
 import Image from "next/image";
-//   import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const TopRatedDoctors = async () => {
   const res = await fetch("http://localhost:5000/api/v1/doctor?page=1&limit=3");
@@ -37,59 +36,59 @@ const TopRatedDoctors = async () => {
         </Typography>
       </Box>
 
-      {/* <Container sx={{ margin: "30px auto" }}>
-          <Grid container spacing={2}>
-            {doctors.map((doctor: any) => (
-              <Grid item key={doctor.id} md={4}>
-                <Card>
-                  <Box>
-                    <Image
-                      src={doctor.profilePhoto}
-                      alt="doctor"
-                      width={500}
-                      height={100}
-                    />
-                  </Box>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {doctor.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {doctor.qualification}, {doctor.designation}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" mt={1}>
-                      <LocationOnIcon /> {doctor.address}
-                    </Typography>
-                  </CardContent>
-                  <CardActions
-                    sx={{
-                      justifyContent: "space-between",
-                      px: 2,
-                      paddingBottom: "20px",
-                    }}
-                  >
-                    <Button>Book Now</Button>
-                    <Button variant="outlined">View Profile</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-          <Box
+      <Container sx={{ margin: "30px auto" }}>
+        <Grid container spacing={3}>
+          {doctors.map((doctor: any) => (
+            <Grid item key={doctor.id} md={4}>
+              <Card>
+                <Box>
+                  <Image
+                    src={doctor.profilePhoto}
+                    alt="doctor"
+                    width={500}
+                    height={100}
+                  />
+                </Box>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {doctor.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {doctor.qualification}, {doctor.designation}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" mt={1}>
+                    <LocationOnIcon /> {doctor.address}
+                  </Typography>
+                </CardContent>
+                <CardActions
+                  sx={{
+                    justifyContent: "space-between",
+                    px: 2,
+                    paddingBottom: "20px",
+                  }}
+                >
+                  <Button>Book Now</Button>
+                  <Button variant="outlined">View Profile</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Box
+          sx={{
+            textAlign: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
             sx={{
-              textAlign: "center",
+              marginTop: "20px",
             }}
           >
-            <Button
-              variant="outlined"
-              sx={{
-                marginTop: "20px",
-              }}
-            >
-              View ALL
-            </Button>
-          </Box>
-        </Container> */}
+            View ALL
+          </Button>
+        </Box>
+      </Container>
     </Box>
   );
 };
