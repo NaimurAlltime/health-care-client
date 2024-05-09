7a943656f6c19ff69442addb691841707d94f3c3
Naimur Rahman <naimur2231@gmail.com>
2024-05-09 04:48:35 -0700

    Retrieve And Decode User Info From Local Storage

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,7 +1,35 @@
 import { authKey } from "@/contants/authkey";
-import { setToLocalStorage } from "@/utils/local-storage";
+import {
+  getFromLocalStorage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
+import { decodedToken } from "@/utils/jwt";
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
   //   console.log(accessToken);
   return setToLocalStorage(authKey, accessToken);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFromLocalStorage(authKey);
+  //   console.log(authToken);
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+    return {
+      ...decodedData,
+      role: decodedData?.role.toLowerCase(),
+    };
+  }
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStorage(authKey);
+  if (authToken) {
+    return !!authToken;
+  }
+};
+
+export const removeUser = () => {
+  return removeFromLocalStorage(authKey);
 };
