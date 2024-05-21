2e12ba563907d748138b991ca4d53a2507701c7e
Naimur Rahman <naimur2231@gmail.com>
2024-05-21 12:09:04 +0600

    Displaying Specialty Data Using Data Grid Table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,10 +1,47 @@
 "use client";
-import { Box, Button, Stack, TextField } from "@mui/material";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialtyModal";
+import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const { data, isLoading } = useGetAllSpecialtiesQuery({});
+
+  const handleDelete = async (id: string) => {};
+
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 400 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      flex: 1,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Image src={row.icon} width={30} height={30} alt="icon" />
+          </Box>
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <IconButton onClick={() => handleDelete(row.id)} aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
 
   return (
     <Box>
@@ -13,6 +50,13 @@ const SpecialtiesPage = () => {
         <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
         <TextField size="small" placeholder="Search Specialist" />
       </Stack>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={data} columns={columns} hideFooter={true} />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
     </Box>
   );
 };
